189044a0abb685a8ec95eb45a91fcc99de86f719
Vidit Gupta <117505027+vidit-04@users.noreply.github.com>
2023-10-08 23:09:52 +0530

    Add files via upload

---
+++ lecture_6I.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int i,j,k;
+    // i=0;
+    // j=0;
+    // k=0;
+    
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // j=i++;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // k=i++;
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+
+    // cout<<i++;
+    // cout<<i;
+    int i;
+    for(i=1;i<101;i++)
+    {
+        cout<<i;
+    }
+}
